7d6dc6343ecc6f7c496477fc89a1b0af85886c91
Tortu <tortuc611@gmail.co>
2020-10-07 03:20:41 +0800

    professional create, update finish

--- backend/src/controllers/professional.controller.js
+++ backend/src/controllers/professional.controller.js
@@ -3,11 +3,24 @@ const Professional = require('../models/professional.mode');
 
 exports.getAll = function (req, res) {
     Professional.getAll(function(err, professionals) {
-        console.log('controller')
         if (err) res.json({
             message: err.message
         })
-        console.log('res', professionals)
         res.send({professionals})
     });
+}
+exports.addNew = function (req, res) {
+
+    Professional.addNew(req.body, (err, result) => {
+        console.log(err)
+        if (err) res.json({message: err.message})
+        if (result.length > 0) res.json({message: "User is already exist. Please enter another email."})
+        else res.json({message: "Success"})
+    })
+}
+exports.update = function (req, res) {
+    Professional.update(req.params.id, req.body, (err, result) => {
+        if (err) res.json({message: err.message})
+        else res.json({message: "Success"})
+    })
 }
--- backend/src/models/professional.mode.js
+++ backend/src/models/professional.mode.js
@@ -12,4 +12,45 @@ Professional.getAll = (result) => {
     })
 }
 
+Professional.addNew = (data, result) => {
+    const values = [data.name, data.gender, data.birthday, data.email, data.comment, data.picture, data.deleted, data.fk_license]
+    const strVal = []
+    values.forEach(val => {
+        strVal.push(JSON.stringify(val))
+    })
+    const sql =  "INSERT INTO professionals(name, gender, birthday, email, comment, picture, deleted, fk_license) VALUES(" + strVal + ")"
+
+
+    dbConn.query("Select * from professionals where email=" + strVal[3], (err, res) => {
+        if (err) result(err.message, null)
+        if (res.length === 0) {
+            dbConn.query(sql,(error, response) => {
+                if (error) result(error.message, null)
+                else result(null, response)
+            })
+        } else {
+            result(null, res)
+        }
+    })
+
+
+}
+
+Professional.update = (id, data, result) => {
+    const values = [data.name, data.gender, data.birthday, data.email, data.comment, data.picture, data.deleted, data.fk_license, id]
+    const strVal = []
+    values.forEach(val => {
+        strVal.push(JSON.stringify(val))
+    })
+    const sql = 'UPDATE professionals SET name='+strVal[0]+', gender='+strVal[1]+', birthday='+strVal[2]+', email='+strVal[3]+
+        ', comment='+strVal[4]+', picture='+strVal[5]+', deleted='+strVal[6]+', fk_license='+strVal[7]+ 'WHERE id='+strVal[8]
+    console.log(sql)
+    dbConn.query(sql, (err, res) => {
+        console.log("error", err)
+        console.log(res)
+        if (err) result(err, null)
+        else result(null, res)
+    })
+}
+
 module.exports = Professional
