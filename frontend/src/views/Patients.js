4c1bb6a827942edb1c711fda57a6e74224149dcc
Tortu <tortuc611@gmail.co>
2020-10-13 00:51:29 +0800

    put tables

--- frontend/src/views/Patients.js
+++ frontend/src/views/Patients.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import {useHistory} from 'react-router-dom'
+import axios from '../services/api'
 import {
   CBadge,
   CCard,
@@ -6,37 +8,144 @@ import {
   CCardHeader,
   CCol,
   CDataTable,
-  CRow
+  CRow,
+  CButton,
 } from '@coreui/react'
+import CIcon from '@coreui/icons-react'
 
-import userGroup from './users/UserGroup'
+import Modal from './Modal'
 
-const getBadge = status => {
-  switch (status) {
-    case 'Active': return 'success'
-    case 'Inactive': return 'secondary'
-    case 'Pending': return 'warning'
-    case 'Banned': return 'danger'
-    default: return 'primary'
+const Patient = () =>{
+  const history = useHistory()
+  const [PatientData, setPatientData] = useState([])
+  const [showModal, setShowModal] = useState(false)
+  const [status, setStatus] = useState(0)
+  const [rowID, setRowID] =useState(null)
+  const fields = ['id','picture', 'name', 'CPF', 'gender', 'birthday', 'email', 'occupation', 'comments', 'fk_license', 'deleted', 'action']
+  const user_info = JSON.parse(localStorage.getItem('user_info'))
+  if (user_info) {
+    const ownRoles = user_info.user.roles.map(item=> item.nome)
+    if (!ownRoles.includes('ROLE_TARGET_EDIT')) fields.splice(-1,1)
   }
-}
-const fields = ['name','registered', 'role', 'status']
-const Professional = () =>{
+
+
+  const hanldeShowModal = () => {
+    setRowID(null)
+    setShowModal(!showModal)
+  }
+
+  const addOrEdit = (id) => {
+    setRowID(id)
+  }
+
+  const deleteRow = (rowID) => {
+    axios.delete('/patients/' + rowID, {
+      headers: {
+        authorization: user_info.accessToken
+      }
+    }).then(res => {
+      alert(res.data.message)
+      handleAddNew()
+    }).catch(err => alert(err.message))
+  }
+
+  useEffect( () => {
+    async function getPermissions() {
+      try {
+        const res = await axios.get('/patients', {
+          headers: {
+            authorization: user_info.accessToken
+          }
+        })
+        if (res.data.patient) {
+          const val = res.data.patient
+          setPatientData(val)
+        } else {
+          history.push('/')
+          localStorage.removeItem('user_info')
+        }
+      } catch (err) {
+        alert(err.message)
+      }
+    }
+    getPermissions()
+  }, [status])
+
+  useEffect(() => {
+    if (rowID !== null){
+      setShowModal(!showModal)
+    }
+  }, [rowID])
+
+  function handleAddNew() {
+    setStatus(status + 1)
+  }
+
   return (
     <>
       <CRow className="justify-content-center">
-        <CCol md="8">
+        <CCol md="12">
           <CCard>
-            <CCardHeader>
-              Simple Table
+            <CCardHeader >
+              <CRow>
+                <CCol>
+                  <CButton onClick={()=>addOrEdit(-1)} className="px-5" color="info">+ Add New</CButton>
+                  <Modal rowID={rowID}  display={showModal} handleDisplay={hanldeShowModal}  handleAddNew={handleAddNew} />
+                </CCol>
+              </CRow>
             </CCardHeader>
             <CCardBody>
-
+              <CDataTable
+                items={PatientData}
+                fields={fields}
+                itemsPerPageSelect
+                itemsPerPage={5}
+                pagination
+                tableFilter
+                scopedSlots = {{
+                  'id' : (item, index) => (
+                    <td>
+                      {index + 1}
+                    </td>
+                  ),
+                  'picture': (item) => (
+                    <td>
+                      <div className={'c-avatar'}>
+                        <img className={'c-avatar-img'} src={item.picture} alt={''}/>
+                      </div>
+                    </td>
+                  ),
+                  'gender':
+                    (item)=>(
+                      <td>
+                        <CBadge shape={'pill'} color={item.gender === 0? 'info' : 'success'}>
+                          {item.gender === 0? "Male" : "Female"}
+                        </CBadge>
+                      </td>
+                    ),
+                  'deleted':(item) => (
+                    <td><CBadge color={item.deleted === 1? 'danger' : 'warning'}>{item.deleted === 1? 'Deleted': 'Working'}</CBadge></td>
+                  ),
+                  'action': (item) => (
+                    <td width={102}>
+                      <CRow>
+                        <CCol>
+                          <CButton onClick={(e)=> addOrEdit(item.id)} className={'btn-pill'} size={'sm'} ><CIcon className={'cust_action_edit'} name={'cilPencil'} /></CButton>
+                        </CCol>
+                        <CCol>
+                          <CButton onClick={(e) => deleteRow(item.id)}  className={'btn-pill'} size={'sm'} ><CIcon className={'cust_action_delete'} name={'cilTrash'}/></CButton>
+                        </CCol>
+                      </CRow>
+                    </td>
+                  )
+                }}
+              />
             </CCardBody>
           </CCard>
         </CCol>
       </CRow>
+
     </>
   );
 }
-export default Professional
+export default Patient
