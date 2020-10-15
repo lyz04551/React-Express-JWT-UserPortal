0d24249c983041e8b5c0c9d139c60a016ce8d9ba
Tortu <tortuc611@gmail.co>
2020-10-15 16:15:28 +0800

    folder structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,10 +12,10 @@ const loading = (
 const TheLayout = React.lazy(() => import('./containers/TheLayout'));
 
 // Pages
-const Login = React.lazy(() => import('./views/pages/login/Login'));
-const Register = React.lazy(() => import('./views/pages/register/Register'));
-const Page404 = React.lazy(() => import('./views/pages/page404/Page404'));
-const Page500 = React.lazy(() => import('./views/pages/page500/Page500'));
+const Login = React.lazy(() => import('./views/auth/login/Login'));
+const Register = React.lazy(() => import('./views/auth/register/Register'));
+const Page404 = React.lazy(() => import('./views/auth/page404/Page404'));
+const Page500 = React.lazy(() => import('./views/auth/page500/Page500'));
 
 class App extends Component {
 
