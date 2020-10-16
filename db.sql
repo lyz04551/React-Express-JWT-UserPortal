0d89be18cfe4afe9c3cc750ae1cf19b92aca488f
Tortu <tortuc611@gmail.co>
2020-10-17 01:46:29 +0800

    db change

--- db.sql
+++ db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Oct 15, 2020 at 03:44 PM
+-- Generation Time: Oct 16, 2020 at 05:45 PM
 -- Server version: 10.4.10-MariaDB
 -- PHP Version: 7.3.12
 
@@ -35,21 +35,20 @@ CREATE TABLE IF NOT EXISTS `categories` (
   `nickname` varchar(20) DEFAULT NULL,
   `amount_patients` int(4) NOT NULL,
   `amount_suitable_overflow` int(2) NOT NULL,
-  `duration_time` datetime DEFAULT NULL,
+  `duration_time` time DEFAULT NULL,
   `color` int(4) NOT NULL,
   `deleted` int(1) NOT NULL DEFAULT 0,
   `fk_license` varchar(25) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `categories`
 --
 
 INSERT INTO `categories` (`id`, `name`, `nickname`, `amount_patients`, `amount_suitable_overflow`, `duration_time`, `color`, `deleted`, `fk_license`) VALUES
-(2, 'customer', 'customer', 23, 234, '2020-10-15 00:00:00', 1, 1, 'asdf'),
-(3, 'sergiy', 'sergiy', 21, 234, '2020-10-06 00:00:00', 2, 1, 'asdf'),
-(5, 'sdf', 'asdf', 21, 2, '2020-10-20 00:00:00', 3, 2, 'adsfadsf');
+(6, 'sergiy', 'sergiy', 3, 3, '17:14:00', 4, 3, 'asdf'),
+(7, 'customer', 'customer', 21, 1, '17:22:00', 2, 1, 'dafadf');
 
 -- --------------------------------------------------------
 
@@ -73,16 +72,14 @@ CREATE TABLE IF NOT EXISTS `licenses` (
   `cat_color_active` int(1) NOT NULL DEFAULT 1,
   `locked` int(1) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `licenses`
 --
 
 INSERT INTO `licenses` (`id`, `name`, `fk_user`, `creation_time`, `expiration_date`, `fixed_time`, `all_markers`, `agenda_interval`, `agenda_start`, `agenda_ending`, `reminder_msg_event`, `cat_color_active`, `locked`) VALUES
-(3, 'sergiy', 'sergiy', '2020-10-14 17:44:52', '2020-10-16 00:00:00', 5.00, 5, 5, '02:44:00', '03:44:00', 'adsf', 4, 1),
-(4, 'sdf', 'sergiy', '2020-10-15 14:10:31', '2020-10-20 00:00:00', 6.00, 7, 4, '22:14:00', '22:14:00', 'adf', 4, 0),
-(5, 'supplier', 'sergiy', '2020-10-15 15:04:51', '2020-10-26 00:00:00', 2.00, 3, 2, '23:06:00', '23:07:00', 'adfa', 2, 1);
+(7, 'sergiy', 'sergiy', '2020-10-16 09:31:09', '2020-10-21 00:00:00', 2.00, 1, 1, '17:33:00', '17:34:00', 'adf', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -128,15 +125,14 @@ CREATE TABLE IF NOT EXISTS `patients` (
   `deleted` int(1) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `idx_1` (`fk_license`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `patients`
 --
 
 INSERT INTO `patients` (`id`, `name`, `CPF`, `gender`, `birthday`, `email`, `occupation`, `comments`, `picture`, `fk_license`, `deleted`) VALUES
-(5, 'customer', 'adfadf', 0, '2020-10-30', 'aaa@gmail.com', 'adfadf', NULL, '', 'adsfadsf', 1),
-(6, 'sergiy', 'adfadf', 1, '2020-10-21', 'sdf@gmail.com', 'adfadf', NULL, '', 'adfadf', 0);
+(7, 'customer', 'adsfads', 1, '2020-10-29', 'customer@admin.com', 'adfadf', NULL, '', 'sdf', 1);
 
 -- --------------------------------------------------------
 
@@ -156,18 +152,15 @@ CREATE TABLE IF NOT EXISTS `professionals` (
   `deleted` int(1) NOT NULL,
   `fk_license` varchar(25) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=51 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=53 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `professionals`
 --
 
 INSERT INTO `professionals` (`id`, `name`, `gender`, `birthday`, `email`, `comment`, `picture`, `deleted`, `fk_license`) VALUES
-(43, 'sergiy', 1, '2020-10-13', 'admin@admin.com', 'adsfas', '', 1, 'asdf'),
-(44, 'roman', 1, '2020-12-01', 'admin@adminsdf.com', 'adsfas', '', 0, 'asdf'),
-(47, 'sergiy', 1, '2020-10-07', 'usfsf@admin.com', 'sdf', '', 0, 'asdf'),
-(49, 'sdf', 1, '2020-10-02', 'tortuc@outlook.com', 'adf', 'C:\\fakepath\\SEO-2.jpg', 1, 'af'),
-(50, 'sergiy', 0, '2020-10-14', 'customer@admin.com', 'asdf', '', 0, 'adf');
+(51, 'sergiy', 1, '2020-10-14', 'customer@admin.com', 'asdf', '', 2, 'adf'),
+(52, 'customer', 2, '2020-10-15', 'aaa@gmail.com', 'sdf', '', 1, 'adf');
 
 -- --------------------------------------------------------
 
@@ -315,16 +308,16 @@ CREATE TABLE IF NOT EXISTS `users` (
   `deleted` int(1) DEFAULT 0,
   `creation_timestamp` timestamp NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `name`, `username`, `email`, `pass`, `initcode`, `cpf`, `birthday`, `gender`, `master`, `active`, `fk_professional`, `fk_license`, `deleted`, `creation_timestamp`) VALUES
-(1, 'admin', 'admin', 'admin@admin.com', '$2b$10$Gm1Ibk1G62qtkFnObtIafOSXCM5fKOub5tt7WUzH3SmrnvIIQgZZi', 'asdfasdf', 'adsf', '2020-12-01', 1, 0, 0, 'adf', 'adf', 0, '2020-10-14 18:21:07'),
-(26, 'customer', 'customer', 'customer@admin.com', '$2b$10$TEMQRD0.BOM6mRFEXtMTQO4KMYOQObdF89fa40d8Q1d7iY469Ms86', 'safa', 'adf', '2020-10-14', 1, 1, 0, 'asdfasdf', 'asdf', 0, '2020-10-14 18:21:09'),
-(28, 'supplier', 'supplier', 'supplier@admin.com', '$2b$10$wZ9iAcPTFaMMKVU4icLopeAcV5JClIRdQu1ldJEsVc0YmcS3lmxzq', 'adfadf', 'adfads', '2020-10-16', 0, 1, NULL, 'asdfasdf', 'asdfadf', 0, '2020-10-15 15:04:26');
+(1, 'admin', 'admin', 'admin@admin.com', '$2b$10$SxqlYQ.IuTilI8c60Y/doe.YZ8nYijAV2qWp/KribUpmgG2XEapLG', 'asdfasdf', 'adsf', '2020-12-01', 1, 1, NULL, 'adf', 'adf', 0, '2020-10-16 15:43:13'),
+(30, 'customer', 'customer', 'customer@admin.com', '$2b$10$/qdQY3tKjwqTROJqjKMzh.L/zk03v/o.Je9/.MNITbi9WyILjxk5q', 'adsfadf', 'adfd', '2020-10-20', 1, 2, NULL, 'adsfadsf', 'adf', 0, '2020-10-16 15:43:58'),
+(34, 'developer', 'developer', 'dev@gmail.com', '$2b$10$qCZP/yUlXrtuO8nuzlgcBOl0rjWdiedmcrkkhL4pBbCjEyy1I6gHy', 'adsf', 'asdfadsf', '2020-10-14', 2, 1, NULL, 'adsfads', 'fadf', 0, '2020-10-16 16:31:59');
 
 -- --------------------------------------------------------
 
@@ -368,8 +361,8 @@ CREATE TABLE IF NOT EXISTS `users_usergroup` (
 
 INSERT INTO `users_usergroup` (`users_id`, `usergroup_id`) VALUES
 (1, 1),
-(26, 46),
-(28, 49);
+(30, 46),
+(34, 49);
 
 --
 -- Constraints for dumped tables
